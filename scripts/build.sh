7ec700cebc367fa5a87543aea696b39fc8c4fdee
J. Kobersky <kober32@gmail.com>
2020-05-29 14:56:17 +0200

    Fixed gitignore

---
+++ scripts/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e # stop sript when error occures
+set -u # stop when undefined variable is used
+#set -x # print all execution (good for debugging)
+
+SCRIPT_FOLDER=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+pushd "${SCRIPT_FOLDER}/.."
+sh cart-update.sh
+popd
