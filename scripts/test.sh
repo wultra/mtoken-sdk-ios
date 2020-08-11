06a8a5f58f9c1a62c89d1a27a380a1bfac76c086
Jan Kobersky <kober32@gmail.com>
2020-08-11 14:34:11 +0200

    Integration tests (#26)

--- scripts/test.sh
+++ scripts/test.sh
@@ -6,7 +6,15 @@ set -u # stop when undefined variable is used
 
 SCRIPT_FOLDER=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-DESTINATION="platform=iOS Simulator,OS=13.4.1,name=iPhone 11"
+DESTINATION="platform=iOS Simulator,OS=13.5,name=iPhone SE (2nd generation)"
+PA_URL=""
+ER_URL=""
+OP_URL=""
+NS_URL=""
+APPKEY=""
+APPSECRET=""
+MASTERSPK=""
+APPID=""
 
 # Parse parameters of this script
 while [[ $# -gt 0 ]]
@@ -14,6 +22,46 @@ do
 	case "$1" in
 		-destination)
 			DESTINATION="$2"
+			shift
+			shift
+			;;
+		-pa)
+			PA_URL="$2"
+			shift
+			shift
+			;;
+		-er)
+			ER_URL="$2"
+			shift
+			shift
+			;;
+		-op)
+			OP_URL="$2"
+			shift
+			shift
+			;;
+		-ns)
+			NS_URL="$2"
+			shift
+			shift
+			;;
+		-appkey)
+			APPKEY="$2"
+			shift
+			shift
+			;;
+		-appsecret)
+			APPSECRET="$2"
+			shift
+			shift
+			;;
+		-masterspk)
+			MASTERSPK="$2"
+			shift
+			shift
+			;;
+		-appid)
+			APPID="$2"
 			shift
 			shift
 			;;
@@ -30,11 +78,26 @@ popd
 
 pushd "${SCRIPT_FOLDER}/.."
 
+rm -rf "build" # clear build folder
+
+echo """{
+    \"paServerUrl\"           : \"${PA_URL}\",
+    \"nextStepServerUrl\"     : \"${NS_URL}\",
+    \"enrollmentServerUrl\"   : \"${ER_URL}\",
+    \"operationsServerUrl\"   : \"${OP_URL}\",
+    \"appKey\"                : \"${APPKEY}\",
+    \"appSecret\"             : \"${APPSECRET}\",
+    \"masterServerPublicKey\" : \"${MASTERSPK}\",
+    \"appId\"                 : \"${APPID}\"
+}""" > "WultraMobileTokenSDKTests/Configs/config.json"
+
 xcrun xcodebuild \
+	-derivedDataPath "build" \
     -project "WultraMobileTokenSDK.xcodeproj" \
     -scheme "WultraMobileTokenSDKTests" \
-    -configuration "Release" \
     -destination "${DESTINATION}" \
+    -parallel-testing-enabled NO \
+    -configuration "Debug" \
     test | xcpretty
 
 popd
