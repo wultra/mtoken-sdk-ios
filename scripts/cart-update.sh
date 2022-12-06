d48616762be62429228d09ac15cd80f14fe1bd38
Juraj Ďurech <durech.juraj@gmail.com>
2022-12-06 15:37:05 +0100

    Use "Debug" build for carthage dependencies

--- scripts/cart-update.sh
+++ scripts/cart-update.sh
@@ -5,5 +5,5 @@ set -e # stop sript when error occures
 SCRIPT_FOLDER=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 pushd "${SCRIPT_FOLDER}/.."
-carthage bootstrap --use-xcframeworks --use-submodules --platform ios
+carthage bootstrap --use-xcframeworks --use-submodules --platform ios --configuration Debug
 popd
