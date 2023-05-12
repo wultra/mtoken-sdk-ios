c1f933a98e77fea7f25dc067b4f64bcdbb2704b2
Hopsaheysa <stranskymarek90@gmail.com>
2023-05-12 13:29:07 +0200

    Add Generic payload with custom decoder

--- WultraMobileTokenSDK/Operations/Model/UserOperation/Screens/WMTPostApprovaScreenGeneric.swift
+++ WultraMobileTokenSDK/Operations/Model/UserOperation/Screens/WMTPostApprovaScreenGeneric.swift
@@ -23,17 +23,135 @@ public class WMTPostApprovalScreenGeneric: WMTPostApprovalScreen {
     
     /// Message to the user
     public let message: String
+    
+    /// Payload with data about action after the operation
+    public let payload: GenericPostApprovalScreenPayload
 
     // MARK: Internals
     
     private enum Keys: String, CodingKey {
-        case heading, message
+        case heading, message, payload
     }
 
     public required init(from decoder: Decoder) throws {
         let c = try decoder.container(keyedBy: Keys.self)
         heading = try c.decode(String.self, forKey: .heading)
         message = try c.decode(String.self, forKey: .message)
+        payload = try c.decode(GenericPostApprovalScreenPayload.self, forKey: .payload)
         try super.init(from: decoder)
+    }
+    
+    public init(heading: String, message: String, payload: GenericPostApprovalScreenPayload) {
+        self.heading = heading
+        self.message = message
+        self.payload = payload
+        super.init(type: .generic)
+    }
+}
+
+///Payload of the generic post-approval screen may contain any object.
+///
+///So it implements custome JSONValue for decoding generic response
+public class GenericPostApprovalScreenPayload: PostApprovalScreenPayload {
+    
+    /// Generic Custom message may contain any object, kept to be String
+    public let customMessage: JSONValue
+    
+    // MARK: Internals
+    
+    private enum Keys: String, CodingKey {
+        case customMessage
+    }
+    
+    public init(customMessage: JSONValue) throws {
+        self.customMessage = customMessage
+        super.init()
+    }
+    
+    required init(from decoder: Decoder) throws {
+        let c = try decoder.container(keyedBy: Keys.self)
+        customMessage = try c.decode(JSONValue.self, forKey: .customMessage)
+        super.init()
+    }
+    
+    public enum JSONValue: Codable, Equatable {
+        case string(String)
+        case int(Int)
+        case double(Double)
+        case bool(Bool)
+        case object([String: JSONValue])
+        case array([JSONValue])
+        case null
+        
+        public subscript(key: String) -> JSONValue? {
+            if case .object(let object) = self {
+                return object[key]
+            }
+            return nil
+        }
+        
+        public init(from decoder: Decoder) throws {
+            if let c = try? decoder.singleValueContainer(),
+               let string = try? c.decode(String.self) {
+                self = .string(string)
+            } else if let c = try? decoder.container(keyedBy: AnyCodingKey.self) {
+                var object = [String: JSONValue]()
+                for key in c.allKeys {
+                    object[key.stringValue] = try c.decode(JSONValue.self, forKey: key)
+                }
+                self = .object(object)
+            } else if var c = try? decoder.unkeyedContainer() {
+                var array = [JSONValue]()
+                while !c.isAtEnd {
+                    array.append(try c.decode(JSONValue.self))
+                }
+                self = .array(array)
+            } else if let c = try? decoder.singleValueContainer() {
+                if c.decodeNil() {
+                    self = .null
+                } else if let bool = try? c.decode(Bool.self) {
+                    self = .bool(bool)
+                } else if let int = try? c.decode(Int.self) {
+                    self = .int(int)
+                } else if let double = try? c.decode(Double.self) {
+                    self = .double(double)
+                } else if let string = try? c.decode(String.self) {
+                    self = .string(string)
+                } else {
+                    let data = try JSONSerialization.data(withJSONObject: decoder)
+                    if let string = String(data: data, encoding: .utf8) {
+                        self = .object(["error": .string("Unknown JSON pattern"), "json": .string(string)])
+                    } else {
+                        self = .object(["error": .string("Unknown JSON pattern"), "json": .null])
+                    }
+                }
+            } else {
+                let data = try JSONSerialization.data(withJSONObject: decoder)
+                if let string = String(data: data, encoding: .utf8) {
+                    self = .object(["error": .string("Unknown JSON pattern"), "json": .string(string)])
+                } else {
+                    self = .object(["error": .string("Unknown JSON pattern"), "json": .null])
+                }
+            }
+        }
+    }
+    
+    /// Helper struct for key: value decoding
+    struct AnyCodingKey: CodingKey {
+        
+        /// property to hold the key's name as a string
+        var stringValue: String
+        
+        /// optional property to hold the key's index if it's an array index.
+        var intValue: Int?
+        
+        init?(stringValue: String) {
+            self.stringValue = stringValue
+        }
+        
+        init?(intValue: Int) {
+            self.stringValue = String(intValue)
+            self.intValue = intValue
+        }
     }
 }
