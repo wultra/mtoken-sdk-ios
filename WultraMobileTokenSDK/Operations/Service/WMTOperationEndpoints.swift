77e2fb6a9c7ff3d8fcb9d36b8e0c638aba9b52e4
Marek Stransky <stranskymarek90@gmail.com>
2025-01-24 10:59:57 +0100

    Remove unused generics in operation expoints

--- WultraMobileTokenSDK/Operations/Service/WMTOperationEndpoints.swift
+++ WultraMobileTokenSDK/Operations/Service/WMTOperationEndpoints.swift
@@ -19,13 +19,13 @@ import WultraPowerAuthNetworking
 
 enum WMTOperationEndpoints {
     
-    enum List<T: WMTUserOperation> {
-        typealias EndpointType = WPNEndpointSignedWithToken<WPNRequestBase, WMTOperationListResponse<T>>
+    enum List {
+        typealias EndpointType = WPNEndpointSignedWithToken<WPNRequestBase, WMTOperationListResponse<WMTUserOperation>>
         static var endpoint: EndpointType { WPNEndpointSignedWithToken(endpointURLPath: "/api/auth/token/app/operation/list", tokenName: "possession_universal") }
     }
     
-    enum History<T: WMTUserOperation> {
-        typealias EndpointType = WPNEndpointSigned<WPNRequestBase, WPNResponseArray<T>>
+    enum History {
+        typealias EndpointType = WPNEndpointSigned<WPNRequestBase, WPNResponseArray<WMTUserOperation>>
         static var endpoint: EndpointType { WPNEndpointSigned(endpointURLPath: "/api/auth/token/app/operation/history", uriId: "/operation/history") }
     }
     
@@ -39,13 +39,13 @@ enum WMTOperationEndpoints {
         static let endpoint: EndpointType = WPNEndpointSigned(endpointURLPath: "/api/auth/token/app/operation/cancel", uriId: "/operation/cancel")
     }
     
-    enum OperationDetail<T: WMTUserOperation> {
-        typealias EndpointType = WPNEndpointSignedWithToken<WPNRequest<WMTOperationDetailRequest>, WPNResponse<T>>
+    enum OperationDetail {
+        typealias EndpointType = WPNEndpointSignedWithToken<WPNRequest<WMTOperationDetailRequest>, WPNResponse<WMTUserOperation>>
         static var endpoint: EndpointType { WPNEndpointSignedWithToken(endpointURLPath: "/api/auth/token/app/operation/detail", tokenName: "possession_universal") }
     }
     
-    enum OperationClaim<T: WMTUserOperation> {
-        typealias EndpointType = WPNEndpointSignedWithToken<WPNRequest<WMTOperationDetailRequest>, WPNResponse<T>>
+    enum OperationClaim {
+        typealias EndpointType = WPNEndpointSignedWithToken<WPNRequest<WMTOperationDetailRequest>, WPNResponse<WMTUserOperation>>
         static var endpoint: EndpointType { WPNEndpointSignedWithToken(endpointURLPath: "/api/auth/token/app/operation/detail/claim", tokenName: "possession_universal") }
     }
 }
