d0a6320144087307a34da7386b86b6f6fdf530df
Marek Stransky <stranskymarek90@gmail.com>
2025-09-09 17:29:11 +0200

    Improve decoding of test data

--- WultraMobileTokenSDK/Operations/Model/UserOperation/WMTOperationUIData.swift
+++ WultraMobileTokenSDK/Operations/Model/UserOperation/WMTOperationUIData.swift
@@ -48,20 +48,19 @@ open class WMTOperationUIData: Codable {
         let c = try decoder.container(keyedBy: Keys.self)
         flipButtons = try? c.decode(Bool.self, forKey: .flipButtons)
         blockApprovalOnCall = try? c.decode(Bool.self, forKey: .blockApprovalOnCall)
-        // TODO: REMOVE when BE is finalized
-        preApprovalScreens = (try? c.decode([WMTPreApprovalScreen].self, forKey: .preApprovalScreens)) ?? Self.defaultPreApprovalScreensProvider?()
+        // TODO: REMOVE defaultPreApprovalScreensProvider when BE is finalized
+        let predefinedTestPreApproval = Self.defaultPreApprovalScreensProvider?()
         
         // 1) New plural
-//        if let screens = try? c.decode([WMTPreApprovalScreen].self, forKey: .preApprovalScreens) {
-//            preApprovalScreens = screens
-//        
-//        // 2) If legacy singular is in the payload map to plural
-//        } else if c.contains(.preApprovalScreenLegacy), let legacyDecoder = try? c.superDecoder(forKey: .preApprovalScreenLegacy) {
-//            preApprovalScreens = WMTPreApprovalScreen.fromLegacy(legacyDecoder)
-//        } else {
-//            preApprovalScreens = nil
-//        }
-//        
+        if let screens = (try? c.decode([WMTPreApprovalScreen].self, forKey: .preApprovalScreens)) ?? predefinedTestPreApproval {
+            preApprovalScreens = screens
+        // 2) If legacy singular is in the payload map to plural
+        } else if c.contains(.preApprovalScreenLegacy), let legacyDecoder = try? c.superDecoder(forKey: .preApprovalScreenLegacy) {
+            preApprovalScreens = WMTPreApprovalScreen.fromLegacy(legacyDecoder)
+        } else {
+            preApprovalScreens = nil
+        }
+        
         postApprovalScreen = try? c.decode(WMTPostApprovalScreenDecodable.self, forKey: .postApprovalScreen).postApprovalObject
     }
     
