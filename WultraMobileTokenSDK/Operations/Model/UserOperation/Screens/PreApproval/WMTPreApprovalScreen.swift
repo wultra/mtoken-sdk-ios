0c27869a7ff2eb34df3885d88c82fa739ca53641
Marek Stransky <stranskymarek90@gmail.com>
2025-10-06 12:40:47 +0200

    Add fallback icon to legacy payload

--- WultraMobileTokenSDK/Operations/Model/UserOperation/Screens/PreApproval/WMTPreApprovalScreen.swift
+++ WultraMobileTokenSDK/Operations/Model/UserOperation/Screens/PreApproval/WMTPreApprovalScreen.swift
@@ -139,7 +139,7 @@ public extension WMTPreApprovalScreen {
                 guard c.contains(.items) else { return nil }
                 let items = (try? c.decode([String].self, forKey: .items)) ?? []
                 guard !items.isEmpty else { return nil }
-                return items.map { WMTPreApprovalElementListItem(text: $0) }
+                return items.map { WMTPreApprovalElementListItem(icon: "fallback_icon", text: $0) }
             }()
 
             // controls only if approvalType == SLIDER
