020305677d906fd87577f097a2d422ddf0306a31
Marek Stransky <stranskymarek90@gmail.com>
2025-10-08 07:50:01 +0200

    Add new rejection reason

--- WultraMobileTokenSDKTests/IntegrationTests.swift
+++ WultraMobileTokenSDKTests/IntegrationTests.swift
@@ -427,7 +427,7 @@ class IntegrationTests: XCTestCase {
                         "test4": ["nested": true]
                     ]
 
-                    self.ops.reject(operation: opToReject, with: .custom("POSSIBLE_FRAUD")) { result in
+                    self.ops.reject(operation: opToReject, with: .preapproval) { result in
                         switch result {
                         case .success:
                             
