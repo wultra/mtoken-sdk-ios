7e354f8ac8c452508e5bef41ecfdf7d1693a5fb2
Jan Kobersky <kober32@gmail.com>
2026-05-15 11:05:08 +0200

    Using networking-android @ develop

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         // PowerAuth Mobile SDK 2.0+ provides full SPM support directly from its main repository.
         // The `PowerAuthCore` module is no longer exposed; all functionality is provided by `PowerAuth2`.
         .package(url: "https://github.com/wultra/powerauth-mobile-sdk.git", branch: "develop"),
-        .package(url: "https://github.com/wultra/networking-apple.git", branch: "issues/79-pa-integration")
+        .package(url: "https://github.com/wultra/networking-apple.git", branch: "develop")
     ],
     targets: [
         .target(
